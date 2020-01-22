b0460eff53b64a45deb78d6adab1775e4528ec54
Kristoffer Benjaminsen <33286455+Lillebenn@users.noreply.github.com>
2020-01-22 14:42:26 +0100

    Added projectilemovement and its defaults
    also some minor comment fixes

--- Source/FrogGame/TongueProjectile.cpp
+++ Source/FrogGame/TongueProjectile.cpp
@@ -10,7 +10,7 @@ ATongueProjectile::ATongueProjectile() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Create the root CapsuleComponent to handle the pickup's collision
+	// Create the root CapsuleComponent to handle the Tongue's collision
 	BaseCollisionComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("BaseCapsuleComponent"));
 
 	// Set the SphereComponent as the root component.
@@ -20,6 +20,12 @@ ATongueProjectile::ATongueProjectile() {
 	TongueMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TongueMesh"));
 	TongueMesh->SetSimulatePhysics(false);
 	TongueMesh->AttachTo(RootComponent);
+
+	// Creates the projectileMovement component and set some defaults.
+	TongueProjectile = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("TongueProjectile"));
+	TongueProjectile->InitialSpeed = 5000;
+	TongueProjectile->MaxSpeed = 5000;
+	TongueProjectile->Velocity.X = 5000;
 }
 
 // Called when the game starts or when spawned
--- Source/FrogGame/TongueProjectile.h
+++ Source/FrogGame/TongueProjectile.h
@@ -19,9 +19,13 @@ public:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Tongue)
 	class UCapsuleComponent* BaseCollisionComponent;
 
-	/** StaticMeshComponent to represent the pickup in the level */
+	/** StaticMeshComponent to represent the Tongue in the level */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Tongue)
 	class UStaticMeshComponent* TongueMesh;
+
+	/** Projectile component **/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Tongue)
+	class UProjectileMovementComponent* TongueProjectile;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
