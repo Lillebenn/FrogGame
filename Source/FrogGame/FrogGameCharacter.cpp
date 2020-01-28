7357c9c34ba87a0d8ce2b0fffa4dfbc48a554da1
Kristoffer Benjaminsen <33286455+Lillebenn@users.noreply.github.com>
2020-01-28 10:29:55 +0100

    Pre-merge commit
    Tongue sticks to targets, needs to only stick to edibles.

--- Source/FrogGame/FrogGameCharacter.cpp
+++ Source/FrogGame/FrogGameCharacter.cpp
@@ -201,7 +201,6 @@ void AFrogGameCharacter::UpdateLength() {
 
 void AFrogGameCharacter::UseTongue() {
 	// Check if there is a CurrentTarget.
-	// Spawn TongueProjectile Actor
 
 	// if there is a CurrentTarget:
 	// {
@@ -220,7 +219,6 @@ void AFrogGameCharacter::UseTongue() {
 	// }
 	
 	// If PowerMeter is full & PowerMode is not already active, activate PowerMode here?
-	// Destroy TongueProjectile Actor
 }
 
 FVector AFrogGameCharacter::GetEnd() {
